c1ae61536a96c825ce7c8e0c9e36bc420b2db766
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2018-12-05 21:01:48 +0000

    Added some classes

---
+++ PhotoOrganiser/Data/SessionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoOrganiser.Data
+{
+    public static class SessionData
+    {
+        public static List<Album> albums { get; set; }
+        public static List<Photo> photos { get; set; }
+
+    }
+}
---
+++ PhotoOrganiser/Data/album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoOrganiser.Data
+{
+    public class Album
+    {
+        public Guid id { get; set; }
+        public string dir { get; set; }
+        public string name { get; set; }
+        public List<Photo> MyProperty { get; set; }
+
+    }
+}
